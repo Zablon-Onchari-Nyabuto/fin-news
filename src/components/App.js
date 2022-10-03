80b9d7147dc42c8bb16dd359b8d6928fce4a4315
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-10-03 23:03:59 +0300

    Update the index.js file after moving app.js from src to components folder

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AccountContainer from "./AccountContainer";
+
+function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h1>FIN-NEWS</h1>
+      </div>
+      <FinnewsContainer />
+    </div>
+  );
+}
+
+export default App;
