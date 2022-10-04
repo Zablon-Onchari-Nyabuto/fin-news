256026d9315eff061ee173f56c7a6e8dae35b6c3
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-10-04 22:18:53 +0300

    Add financialList and Search file to act as child components to Watchlist component

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,8 +1,32 @@
-import React from "react";
-function WatchList (){
-    return(
-        <div></div>
-    );
-}
+import React, { useEffect, useState } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import AddTransactionForm from "./AddTransactionForm";
 
-export default WatchList;
+function AccountContainer() {
+  const [transactionsListed, setTransactionsListed] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((res) => res.json())
+      .then((data) => setTransactionsListed(data));
+    }, []);
+
+  function addTransaction(newTransaction) {
+    const updateTransactions = [...transactionsListed, newTransaction]
+    setTransactionsListed(updateTransactions)
+  }
+  
+  function searchTransaction(newSearch){
+    setSearch(newSearch)
+  }
+
+  return (
+    <div>
+      <Search search={search} searchTrans={searchTransaction} />
+      <AddTransactionForm addTrans={addTransaction}/>
+      <TransactionsList transactionsListed={transactionsListed} search={search} />
+    </div>
+  );
+}
