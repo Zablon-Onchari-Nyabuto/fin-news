24b8686f9c8d7217822b5453f56f958a36e64172
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-10-05 22:12:32 +0300

    Updated WatchList component to correct the routing link provided

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ContactUs from "./ContactUs"
-import Watchlist from './Watchlist';
+import WatchList from './WatchList';
 import AboutUs from './AboutUs';
 import NavBar from "./Navbar";
 import { Route, Routes } from "react-router-dom";
@@ -12,7 +12,7 @@ function App() {
     <div className='App'>
       <NavBar />
       <Routes>
-        <Route path={'/'}  element={<Watchlist />} exact/>
+        <Route path={'/'}  element={<WatchList />} exact />
         <Route path={"/aboutus"} element={<AboutUs />}/>
         <Route path={"/contactus"} element={<ContactUs />} />
       </Routes>
