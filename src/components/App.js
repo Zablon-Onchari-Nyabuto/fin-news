1c158e13caf7dbca32bdb1ffb5a0352462ac65b4
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-10-05 21:57:50 +0300

    Updated aboutus page component with image and content

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,20 @@ import ContactUs from "./ContactUs"
 import Watchlist from './Watchlist';
 import AboutUs from './AboutUs';
 import NavBar from "./Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import 'semantic-ui-css/semantic.min.css';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <BrowserRouter>
     <div className='App'>
       <NavBar />
       <Routes>
-        <Route path={'/'}  element={<Watchlist/>} exact/>
-        <Route path={"/aboutus"} element={<AboutUs/>}/>
+        <Route path={'/'}  element={<Watchlist />} exact/>
+        <Route path={"/aboutus"} element={<AboutUs />}/>
         <Route path={"/contactus"} element={<ContactUs />} />
       </Routes>
     </div>
-    </BrowserRouter>
   );
 }
-
-
 
 export default App;
